42355610fd1b0010ca95b99e9cb62508aa73f2a3
machinelearning-guy <86847808+machinelearning-guy@users.noreply.github.com>
2024-01-09 13:38:45 +0000

    Update DNAtoolkit

--- DNAtoolkit.py
+++ DNAtoolkit.py
@@ -1,1 +1,13 @@
+#dna tool kit file
+
+nucleotides = ["A", "C", "G", "T"]
+
+
+#check the sequence to make sure it is a DNA string
+def validateseq(dna_seq):
+    tmpseq = dna_seq.upper()
+    for nuc in tmpseq:
+        if nuc not in nucleotides:
+            return False
+        return tmpseq
 
