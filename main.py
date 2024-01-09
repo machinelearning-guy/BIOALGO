09aef141ce3c5f1f631c3efcc346f63a6132f1ea
machinelearning-guy <86847808+machinelearning-guy@users.noreply.github.com>
2024-01-09 16:51:49 +0000

    Update main

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ randDNAstr = ''.join([random.choice(nucleotides)
                       for nuc in range(50)])
 
 DNAstr = validateseq(randDNAstr)
-print(countNucFrequency(randDNAstr))
-print(transcription(DNAstr))
+
+
+print(f'\nsequence: {DNAstr}\n')
+print(f'[1] + sequence length: {len(DNAstr)}\n')
+print(f'[2] + sequence length: {countNucFrequency(DNAstr)}\n')
+
+print(f'[3] + DNA/RNA Transcription: {transcription(DNAstr)}\n')
+
+
+
 
 
