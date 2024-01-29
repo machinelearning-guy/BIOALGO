3fa12b895abdeb51ddd55e86508ab65be7b399c4
machinelearning-guy <86847808+machinelearning-guy@users.noreply.github.com>
2024-01-29 12:42:37 +0000

    Create utilities.py

---
+++ utilities.py
@@ -0,0 +1,22 @@
+
+def colored(seq):
+     global nuc
+     bcolors = {
+         'A': '\033[92m',
+         'C': '\033[94m',
+         'G': '\033[93m',
+         'T': '\033[91m',
+         'U': '\033[92m',
+         'reset':  '\033[0:0m'
+     }
+
+     tmpstr = ""
+
+     for nuc in seq:
+         if nuc in bcolors:
+             tmpstr += bcolors[nuc] + nuc
+     else:
+         tmpstr += bcolors['reset'] + nuc
+
+         return tmpstr + '\033[0:0m'
+
